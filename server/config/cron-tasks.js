c977d577825cf9a2c24fa96b7fe0f8edf1772186
Felix Franz <felix.franz@ergosign.de>
2024-01-18 17:01:54 +0100

    added check if cron is enabled
    before registering the cron task

--- server/config/cron-tasks.js
+++ server/config/cron-tasks.js
@@ -6,28 +6,32 @@ module.exports = {
 	registerCronTasks: ({ strapi }) => {
 		const settings = getPluginService('settingsService').get();
 
-		// create cron check
-		strapi.cron.add({
-			publisherCronTask: {
-				options: {
-					rule: settings.actions.syncFrequency,
-				},
-				task: async () => {
-					// fetch all actions that have passed
-					const records = await getPluginService('action').find({
-						filters: {
-							executeAt: {
-								$lte: Date.now(),
-							},
-						},
-					});
+		// only register cron-tasks if cron is enabled:
+		if (strapi.config.get('server.cron.enabled', true)) {
 
-					// process action records
-					for (const record of records.results) {
-						getPluginService('publicationService').toggle(record, record.mode);
-					}
+			// create cron check
+			strapi.cron.add({
+				publisherCronTask: {
+					options: {
+						rule: settings.actions.syncFrequency,
+					},
+					task: async () => {
+						// fetch all actions that have passed
+						const records = await getPluginService('action').find({
+							filters: {
+								executeAt: {
+									$lte: Date.now(),
+								},
+							},
+						});
+
+						// process action records
+						for (const record of records.results) {
+							getPluginService('publicationService').toggle(record, record.mode);
+						}
+					},
 				},
-			},
-		});
+			});
+		}
 	},
 };
