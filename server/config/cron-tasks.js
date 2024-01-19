406c59d2d2d663c5d6f90cb873321fd132790cf4
Felix Franz <felix.franz@ergosign.de>
2024-01-19 09:21:50 +0100

    use guard at top

--- server/config/cron-tasks.js
+++ server/config/cron-tasks.js
@@ -7,31 +7,32 @@ module.exports = {
 		const settings = getPluginService('settingsService').get();
 
 		// only register cron-tasks if cron is enabled:
-		if (strapi.config.get('server.cron.enabled', true)) {
-
-			// create cron check
-			strapi.cron.add({
-				publisherCronTask: {
-					options: {
-						rule: settings.actions.syncFrequency,
-					},
-					task: async () => {
-						// fetch all actions that have passed
-						const records = await getPluginService('action').find({
-							filters: {
-								executeAt: {
-									$lte: Date.now(),
-								},
-							},
-						});
-
-						// process action records
-						for (const record of records.results) {
-							getPluginService('publicationService').toggle(record, record.mode);
-						}
-					},
-				},
-			});
+		if (!strapi.config.get('server.cron.enabled', true)) {
+			console.log("strapi-plugin-publisher: Cron is not enabled, no cron-task registering");
+			return;
 		}
+		// create cron check
+		strapi.cron.add({
+			publisherCronTask: {
+				options: {
+					rule: settings.actions.syncFrequency,
+				},
+				task: async () => {
+					// fetch all actions that have passed
+					const records = await getPluginService('action').find({
+						filters: {
+							executeAt: {
+								$lte: Date.now(),
+							},
+						},
+					});
+
+					// process action records
+					for (const record of records.results) {
+						getPluginService('publicationService').toggle(record, record.mode);
+					}
+				},
+			},
+		});
 	},
 };
